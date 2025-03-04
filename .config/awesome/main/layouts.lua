8beb98ea3895e57a5a1ce9537aa7e3c0e44662f1
FUNCTIAL <77804731+FUNCTIAL@users.noreply.github.com>
2025-03-04 20:33:37 +0700

    Initialize Git

---
+++ .config/awesome/main/layouts.lua
@@ -0,0 +1,12 @@
+local awful = require("awful")
+
+--
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+awful.layout.layouts = {
+    awful.layout.suit.fair,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.floating,
+}
+-- }}}
